09039b337ed1fb33a23a8abe5b24c5bccf9013f9
Kairat Sultan <garri.sultonov@mail.ru>
2022-08-12 19:21:14 +0600

    some changes for debug

--- goldentravel/contrib/context/banner_context.py
+++ goldentravel/contrib/context/banner_context.py
@@ -5,11 +5,13 @@ def get_banner_processor(request):
     pk = request.path.split('/')[-1]
     if not pk:
         tour_image_list = TourBanner.objects.all().order_by('-tour__pk')
-    else:
+    elif pk.isdigit():
         tour_image_list = []
         first = TourBanner.objects.filter(tour__pk=int(pk)).first()
         tour_image_list.append(first)
         another = TourBanner.objects.all().exclude(pk=first.id)
         for i in another:
             tour_image_list.append(i)
+    else:
+        tour_image_list = []
     return {"banner": tour_image_list}
